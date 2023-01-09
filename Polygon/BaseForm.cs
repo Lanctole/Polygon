ed212408bd0d00cd114a7e7b17bc30d5d7a7926b
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-09 17:00:55 +0300

    Feat: Create BaseForm parent class

---
+++ Polygon/BaseForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Polygon
+{
+    public class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            BackColor = Color.Black;
+            TopMost = true;
+            Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            WindowState = FormWindowState.Maximized;
+        }
+    }
+}
