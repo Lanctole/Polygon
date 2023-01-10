f24aa62adefe7691f4d0dcb80ceb3170db116994
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-10 10:22:04 +0300

    Feat: create world class which keeps a lvl to generated things

---
+++ Polygon/World.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon
+{
+    public class World
+    {
+        public int Level { get; set; }
+
+        public World(Character character)
+        {
+            this.Level = 1;
+            character.CharacterLeveledUp += OnCharacterLeveledUp;
+        }
+
+        private void OnCharacterLeveledUp(object sender, EventArgs e)
+        {
+            Level++;
+        }
+    }
+}
