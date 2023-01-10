6cd5a0a88d54d265fa9817e599dcd7243c862914
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-10 12:18:01 +0300

    Feat: add main menu music player

---
+++ Polygon/MenuMusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon
+{
+    internal static class MenuMusicPlayer
+    {
+        public static void Play()
+        {
+            try
+            {
+                SoundPlayer backgroundSound = new SoundPlayer(Properties.Music.MenuSound);
+                backgroundSound.PlayLooping();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Background sound file not found");
+            }
+        }
+    }
+}
