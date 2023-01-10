689ccf870a083b17e255934bfdb1913eed33ea9a
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-10 10:23:50 +0300

    Fix: Character lvl-up lvl-ups the World

--- Polygon/Character.cs
+++ Polygon/Character.cs
@@ -66,7 +66,7 @@ namespace Polygon
                     {
                         this.Experience -= LevelUpThreshold;
                         this.Level++;
-                        CharacterLeveledUp?.Invoke(this, EventArgs.Empty);
+                        World.IncreaseLevel(1);
                     }
                     else
                     {
