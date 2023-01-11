657bb1279e1bfc641687a4d1f31da74b2536420d
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-11 14:35:59 +0300

    Feat: world creation constants

--- Polygon/Entities/World.cs
+++ Polygon/Entities/World.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Polygon
+namespace Polygon.Entities
 {
     public static class World
     {
-        public static int Level { get; set; }
+        public static int Level { get; private set; }
+        public static double WeaponToArmorStrengthAttitude { get; private set; }
 
+        public static int PowerScaling { get; private set; }
+        public static int EnemiesRange { get; private set; }
+
+        static World()
+        {
+            Level = 1;
+            WeaponToArmorStrengthAttitude = 1.2;
+            EnemiesRange = 5;
+            PowerScaling = 10;
+        }
+
+        public static void ChangeWeaponToArmorStrengthAttitude(double coefficient)
+        {
+            WeaponToArmorStrengthAttitude = coefficient;
+        }
+
+        public static void ChangePowerScaling(int multiplier)
+        {
+            PowerScaling = multiplier;
+        }
+
+        public static void ChangeEnemiesRange(int range)
+        {
+            EnemiesRange = range;
+        }
         public static void IncreaseLevel(int amount)
         {
             World.Level += amount;
