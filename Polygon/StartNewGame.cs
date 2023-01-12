d4b6e3d5342d0b66c743a1c5c99d6baed8d59b37
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-12 14:17:09 +0300

    Create Start Game Function

---
+++ Polygon/StartNewGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Polygon.Entities;
+
+namespace Polygon
+{
+    public static class StartGame
+    {
+        public static void StartNewGame()
+        {
+            var character = new Character();
+        }
+    }
+}
