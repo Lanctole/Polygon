4bb5b25cc0672ffdee44c6d43324c9a19e48852a
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-12 12:22:12 +0300

    Create generator what randomize characteristics

---
+++ Polygon/Entities/NumeralCharacteristicsGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon.Entities
+{
+    internal static class NumeralCharacteristicsGenerator
+    {
+        public static int GenerateElementValue()
+        {
+            var random = new Random();
+            return World.Level*World.PowerScaling+random.Next(1, World.PowerScaling*2);
+        }
+
+        public static int GenerateDamageValue()
+        {
+            return Convert.ToInt32(GenerateDefenseValue()*World.WeaponToArmorStrengthAttitude);
+        }
+        public static int GenerateDefenseValue()
+        {
+            var random = new Random();
+            return random.Next(World.Level * World.PowerScaling / 2, World.Level * World.PowerScaling);
+        }
+
+        public static int GenerateCost(Item item)
+        {
+            var random = new Random();
+            int numOfEffects=item.MagicEffect.Elements.Count;
+            var cost =(int)((100 + World.Level)*(1-random.Next(-2,3)/10));
+            if (numOfEffects > 0)
+            {
+                cost*=numOfEffects;
+            }
+            return cost;
+        }
+    }
+}
