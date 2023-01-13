3978452136f34c86a1885bffb5b2e1409a4db04b
55445592+Lanctole@users.noreply.github.com <55445592+Lanctole@users.noreply.github.com>
2023-01-13 12:08:24 +0300

    Create Controls. Through them Form interact with game entities

---
+++ Polygon/Controls/GameSessionControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polygon.Game;
+using Polygon.Game.Entities;
+using Polygon.UI;
+
+namespace Polygon.Controls
+{
+    internal class GameSessionControl
+    {
+        public void NewGame(MainForm mainForm)
+        {
+            var primaryControls = new PrimaryControls(Start.StartNewGame());
+            mainForm.DrawGameScreen(primaryControls);
+        }
+    }
+}
---
+++ Polygon/Controls/PrimaryControls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Polygon.Game.Entities;
+
+namespace Polygon.Controls
+{
+    public  class PrimaryControls
+    {
+        public Character character;
+
+        public PrimaryControls(Character character)
+        {
+            this.character = character;
+        }
+
+        public PropertyInfo[] GetCharacterInfo()
+        {
+            return character.GetType().GetProperties();
+        }
+    }
+}
